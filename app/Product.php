be7bf6112a2866c02447def76bcca1318d90852a
leo-demetrio <leocdemetrio@gmail.com>
2021-01-08 10:44:02 -0300

    finalizando a estrutura do bd

---
+++ app/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+	use SoftDeletes;
+    protected $fillable = [
+    	'category_id','name','description','exclusive'
+    ];
+
+    protected $dates = [
+    	'deleted_at'
+    ];
+
+    public function category()
+    {
+    	return $this->belongsTo(Category::class);
+    }
+}
